4b969e65fa5748930405b927bccff136b1a19ca1
profitsw2000 <profitsw2000@gmail.com>
2021-06-16 18:57:38 +0300

    Исходное состояние приложения с заметками.

---
+++ app/src/main/java/ru/profitsw2000/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.profitsw2000;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.profitsw2000.notes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
