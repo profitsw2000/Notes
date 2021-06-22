49b760dc209436cbefafa071fe4633085fc177ea
profitsw2000 <profitsw2000@gmail.com>
2021-06-22 22:12:44 +0300

    Добавлено стандартное меню, на пункты меню поставлены заглушки в виде всплывающих сообщений.

--- app/src/main/java/ru/profitsw2000/MainActivity.java
+++ app/src/main/java/ru/profitsw2000/MainActivity.java
@@ -3,6 +3,12 @@ package ru.profitsw2000;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.SearchView;
 
 import ru.profitsw2000.notes.R;
 
@@ -12,5 +18,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar()   ;
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar)    ;
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast toast ;
+
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_settings:
+                toast = Toast.makeText(getApplicationContext(),"Settings", Toast.LENGTH_LONG)   ;
+                toast.show();
+                return true;
+            case R.id.action_favorite:
+                toast = Toast.makeText(getApplicationContext(),"Favorite", Toast.LENGTH_LONG)   ;
+                toast.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query,
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        return true;
+    }
+
 }
